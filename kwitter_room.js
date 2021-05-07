f1a633c1342eb594602250f623626b268dba2149
PokemonPro10 <75942534+PokemonPro10@users.noreply.github.com>
2021-05-07 10:28:55 +0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyBSdwwlbeEY0j7tfhuP38omV_zFG08MbvA",
+  authDomain: "letschat-aa217.firebaseapp.com",
+  databaseURL: "https://letschat-aa217-default-rtdb.firebaseio.com",
+  projectId: "letschat-aa217",
+  storageBucket: "letschat-aa217.appspot.com",
+  messagingSenderId: "969303171683",
+  appId: "1:969303171683:web:170733489dd12330df0e56"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
